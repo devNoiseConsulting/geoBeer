ef62f9b478a42d66b350a90df980a5041c028b1a
Michael J. Flynn <flynnmj@devnoise.com>
2016-09-23 13:51:33 -0400

    Clean up
    Added a package.json file. Put more documentation in the README.md.
    Beautified the javascript files.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # geoBeer
 Keeping track of Brewery visits with GeoJSON
+
+## Introduction
+
+A simple git repository for tracking brewery visits. The brewery files are [GeoJSON files](http://geojson.org/geojson-spec.html)  using the Point geometry object. Most meta data and styling information is stored on the properties object.
+
+
+The marker-color and marker-size properties are used to indicate the visitation status of the brewery.
+
+| Status |Color|Hex Code|Size|
+|---|---|---|---|---|
+|visited|blue|#0047AB|large|
+|visiting soon|green|#009688|medium|
+|to be visited|gray|#607D8B|small|
+
+A couple of javascript files are available to assist in creating and merging breweries.
+
+## Build
+
+You need to have [Node.js](https://nodejs.org/) and [npm](https://www.npmjs.com/) installed. Type `npm run merge` to merge all the brewery GeoJSON files into myBreweryList.geojson.
--- bin/parseBreweries.js
+++ bin/parseBreweries.js
@@ -2,60 +2,64 @@ var fs = require('fs');
 
 var fileName = process.argv[2];
 if (fileName) {
-    var text = fs.readFileSync(fileName, "utf8");
+  var text = fs.readFileSync(fileName, "utf8");
 
-    var breweries = text.toString().split("\n");
-    if (breweries[0].startsWith("Brewery\t")) {
-        breweries = breweries.slice(1);
-    }
+  var breweries = text.toString().split("\n");
+  if (breweries[0].startsWith("Brewery\t")) {
+    breweries = breweries.slice(1);
+  }
 
-    breweries = breweries.filter(function(n){ return n != undefined });
-    breweries = breweries.map(createBrewery);
-    breweries.forEach(writeBrewery);
+  breweries = breweries.filter(function(n) {
+    return n != undefined
+  });
+  breweries = breweries.map(createBrewery);
+  breweries.forEach(writeBrewery);
 } else {
-    function getFileName(path) {
-        return path.substring(path.lastIndexOf('/') + 1, path.length);
-    }
+  function getFileName(path) {
+    return path.substring(path.lastIndexOf('/') + 1, path.length);
+  }
 
-    var node = getFileName(process.argv[0]);
-    var script = getFileName(process.argv[1]);
+  var node = getFileName(process.argv[0]);
+  var script = getFileName(process.argv[1]);
 
-    console.log("Usage: " + node + " " + script + " <filename> ");
-    console.log("\nExpects a tab delimited text file following the following column format:");
-    console.log("1) Brewery\n2) Address\n3) GeoLat\n4) GeoLong\n5) WebsiteUrl");
+  console.log("Usage: " + node + " " + script + " <filename> ");
+  console.log("\nExpects a tab delimited text file following the following column format:");
+  console.log("1) Brewery\n2) Address\n3) GeoLat\n4) GeoLong\n5) WebsiteUrl");
 }
 
 function writeBrewery(brewery) {
-    var newFileName = brewery.features[0].properties.name.replace(/\s+/gi, '-'); // Replace white space with dash
-    newFileName = newFileName.replace(/[^a-zA-Z0-9\-]/gi, ''); // Strip any special characters
-    newFileName = newFileName.replace(/-+/gi, '-'); // Strip any special characters
-    if (newFileName && brewery.features[0].geometry.coordinates[0] && brewery.features[0].geometry.coordinates[1]) {
-        newFileName = "./breweries/" + newFileName + ".geojson";
-        console.log(newFileName);
+  var newFileName = brewery.features[0].properties.name.replace(/\s+/gi, '-'); // Replace white space with dash
+  newFileName = newFileName.replace(/[^a-zA-Z0-9\-]/gi, ''); // Strip any special characters
+  newFileName = newFileName.replace(/-+/gi, '-'); // Strip any special characters
+  if (newFileName &&
+    brewery.features[0].geometry.coordinates[0] &&
+    brewery.features[0].geometry.coordinates[1]) {
+    newFileName = "./breweries/" + newFileName + ".geojson";
+    console.log(newFileName);
 
-        var out = fs.createWriteStream(newFileName, {
-            encoding: "utf8"
-        });
-        out.write(JSON.stringify(brewery, null, 2));
-        out.write("\n");
-        out.end(); // currently the same as destroy() and destroySoon()
-    }
+    var out = fs.createWriteStream(newFileName, {
+      encoding: "utf8"
+    });
+    out.write(JSON.stringify(brewery, null, 2));
+    out.write("\n");
+    out.end(); // currently the same as destroy() and destroySoon()
+  }
 }
 
 
 function createBrewery(dataString) {
-    var data = dataString.split("\t");
-    console.log(dataString);
+  var data = dataString.split("\t");
+  console.log(dataString);
 
-    var fileName = "./breweries/brewery-sample.json";
-    var text = fs.readFileSync(fileName, "utf8");
-    var brewery = JSON.parse(text);
+  var fileName = "./breweries/brewery-sample.json";
+  var text = fs.readFileSync(fileName, "utf8");
+  var brewery = JSON.parse(text);
 
-    brewery.features[0].properties.name = data[0];
-    brewery.features[0].properties.address = data[1];
-    brewery.features[0].geometry.coordinates = [Number(data[3]), Number(data[2])];
-    brewery.features[0].properties.url = data[4];
+  brewery.features[0].properties.name = data[0];
+  brewery.features[0].properties.address = data[1];
+  brewery.features[0].geometry.coordinates = [Number(data[3]), Number(data[2])];
+  brewery.features[0].properties.url = data[4];
 
-    return brewery;
+  return brewery;
 
 }
